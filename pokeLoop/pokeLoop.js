f1b03a50b80fc4aede4a1989ba98d2c94c85ddae
coriandar <88608040+coriandar@users.noreply.github.com>
2023-01-11 03:25:30 +1300

    Create pokeLoop project

---
+++ pokeLoop/pokeLoop.js
@@ -0,0 +1,7 @@
+// TODO:
+// between numbers
+// max load
+
+
+const containter = document.querySelector('#container');
+const baseURL ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'

199b6c6ea0cd238dac9fbb64d16283f997b25985
coriandar <88608040+coriandar@users.noreply.github.com>
2023-01-11 23:19:28 +1300

    Add remove function

--- pokeLoop/pokeLoop.js
+++ pokeLoop/pokeLoop.js
@@ -1,10 +1,22 @@
 // TODO:
 // get value from form
 function populatePokedex () {
-    const pokemonName = ['Bulbasaur', 'Ivysaur', 'Venusaur', 
-                    'Charmandar', 'Charmeleon', 'Charizard',
-                    'Squirtle', 'Wartortle', 'Blastoise',
-                    'Caterpie', 'Metapod', 'Butterfree'];
+    const pokeNames = [
+        'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon' ,'Charizard' ,'Squirtle' ,'Wartortle' ,'Blastoise' ,'Caterpie'
+        ,'Metapod' ,'Butterfree' ,'Weedle' ,'Kakuna' ,'Beedrill' ,'Pidgey' ,'Pidgeotto' ,'Pidgeot' ,'Rattata' ,'Raticate'
+        ,'Spearow' ,'Fearow' ,'Ekans' ,'Arbok' ,'Pikachu' ,'Raichu' ,'Sandshrew' ,'Sandslash' ,'Nidoran' ,'Nidorina' ,'Nidoqueen'
+        ,'Nidoran' ,'Nidorino' ,'Nidoking' ,'Clefairy' ,'Clefable' ,'Vulpix' ,'Ninetales' ,'Jigglypuff' ,'Wigglytuff' ,'Zubat'
+        ,'Golbat' ,'Oddish' ,'Gloom' ,'Vileplume' ,'Paras' ,'Parasect' ,'Venonat' ,'Venomoth' ,'Diglett' ,'Dugtrio'
+        ,'Meowth' ,'Persian' ,'Psyduck' ,'Golduck' ,'Mankey' ,'Primeape' ,'Growlithe' ,'Arcanine' ,'Poliwag' ,'Poliwhirl' ,'Poliwrath'
+        ,'Abra' ,'Kadabra' ,'Alakazam' ,'Machop' ,'Machoke' ,'Machamp' ,'Bellsprout' ,'Weepinbell' ,'Victreebel' ,'Tentacool' ,'Tentacruel'
+        ,'Geodude' ,'Graveler' ,'Golem' ,'Ponyta' ,'Rapidash' ,'Slowpoke' ,'Slowbro' ,'Magnemite' ,'Magneton' ,'Farfetchd' ,'Doduo'
+        ,'Dodrio' ,'Seel' ,'Dewgong' ,'Grimer' ,'Muk' ,'Shellder' ,'Cloyster' ,'Gastly' ,'Haunter' ,'Gengar' ,'Onix'
+        ,'Drowzee' ,'Hypno' ,'Krabby' ,'Kingler' ,'Voltorb' ,'Electrode' ,'Exeggcute' ,'Exeggutor' ,'Cubone' ,'Marowak' ,'Hitmonlee'
+        ,'Hitmonchan' ,'Lickitung' ,'Koffing' ,'Weezing' ,'Rhyhorn' ,'Rhydon' ,'Chansey' ,'Tangela' ,'Kangaskhan' ,'Horsea' ,'Seadra'
+        ,'Goldeen' ,'Seaking' ,'Staryu' ,'Starmie' ,'Mr. Mime' ,'Scyther' ,'Jynx' ,'Electabuzz' ,'Magmar' ,'Pinsir'
+        ,'Tauros' ,'Magikarp' ,'Gyarados' ,'Lapras' ,'Ditto' ,'Eevee' ,'Vaporeon' ,'Jolteon' ,'Flareon' ,'Porygon' ,'Omanyte'
+        ,'Omastar' ,'Kabuto' ,'Kabutops' ,'Aerodactyl' ,'Snorlax' ,'Articuno' ,'Zapdos' ,'Moltres' ,'Dratini' ,'Dragonair' ,'Dragonite', 'Mewtwo'
+        ,'Mew'];
 
     let min = parseInt(document.querySelector('input[name="min"]').value);
     let max = parseInt(document.querySelector('input[name="max"]').value);
@@ -22,7 +34,6 @@ function populatePokedex () {
         max = 151;
     }
 
-    const container = document.querySelector('#container');
     const baseURL ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
     for (let i = min; i <= max; i++){
@@ -32,29 +43,27 @@ function populatePokedex () {
 
         pokemon.classList.add('pokemon');
         label.innerText = `#${i}`;
-        label.innerText += `\n${pokemonName[i-1]}`;
+        label.innerText += `\n${pokeNames[i-1]}`;
         img.src = `${baseURL}${i}.png`;
 
         pokemon.appendChild(img);
         pokemon.appendChild(label);
-        container.appendChild(pokemon);
+        document.body.appendChild(pokemon);
     }
-    /*
-    <section>
-        <div>
-            <img></img>
-            <span>i</span>
-        </div> 
-        <div>
-            <img></img>
-            <span>i</span>
-        </div> 
-    </section>
-    */
+}
+
+function removePokemon() {
+    const div = document.querySelectorAll('.pokemon');
+    if (div.length > 0 ) {
+        for (let ele of div) {
+            ele.remove();
+        }
+    }
 }
 
 const showPokemon = document.querySelector('button');
 
 showPokemon.addEventListener('click', function() {
+    removePokemon();
     populatePokedex()
 });

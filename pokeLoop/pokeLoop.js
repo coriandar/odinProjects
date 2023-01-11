0f254b188878a4005daddcea37774cff13b39ab7
coriandar <88608040+coriandar@users.noreply.github.com>
2023-01-11 17:12:43 +1300

    Add min-max prompt based loop

--- pokeLoop/pokeLoop.js
+++ pokeLoop/pokeLoop.js
@@ -2,6 +2,26 @@
 // between numbers
 // max load
 
-
-const containter = document.querySelector('#container');
+const container = document.querySelector('#container');
 const baseURL ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+// let start = document.querySelector('input[name="start"]').value;
+// console.log(start)
+
+// loop error without parseInt, need sanitise
+let min = parseInt(prompt("min#:"));
+let max = parseInt(prompt("max#: "));
+
+if (min > max) {
+    min = 1;
+}
+
+if (max > 151) {
+    max = 151;
+}
+
+for (let i = min; i <= max; i++){
+    const pokemon = document.createElement('img');
+    pokemon.src = `${baseURL}${i}.png`;
+    container.append(pokemon);
+}

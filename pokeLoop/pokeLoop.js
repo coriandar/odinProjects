2b56a39279872394bb7ba6e72c7872fac5a84b0b
coriandar <88608040+coriandar@users.noreply.github.com>
2023-01-11 21:23:29 +1300

    Add pokemon name

--- pokeLoop/pokeLoop.js
+++ pokeLoop/pokeLoop.js
@@ -1,40 +1,38 @@
 // TODO:
-// add name
+// get value from form
 
-function populatePokedex() {
-    let min = parseInt(document.querySelector('input[name="min"]').value);
-    let max = parseInt(document.querySelector('input[name="max"]').value);
+const pokemonName = ['Bulbasaur', 'Ivysaur', 'Venusaur', 
+                'Charmandar', 'Charmeleon', 'Charizard',
+                'Squirtle', 'Wartortle', 'Blastoise',
+                'Caterpie', 'Metapod', 'Butterfree'];
 
-    if (min > max) {
-        min = 1;
-    }
-    if (max > 151) {
-        max = 151;
-    }
+let min = parseInt(document.querySelector('input[name="min"]').value);
+let max = parseInt(document.querySelector('input[name="max"]').value);
 
-    addPokedexEntry(min, max);
+if (min > max) {
+    min = 1;
+}
+if (max > 151) {
+    max = 151;
 }
 
-function addPokedexEntry(min, max) {
-    const container = document.querySelector('#container');
-    const baseURL ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const container = document.querySelector('#container');
+const baseURL ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
-    for (let i = min; i <= max; i++){
-        const pokemon = document.createElement('div');
-        const label = document.createElement('span');
-        const img = document.createElement('img');
-    
-        pokemon.classList.add('pokemon');
-        label.innerText = `#${i}`;
-        img.src = `${baseURL}${i}.png`;
+for (let i = min; i <= max; i++){
+    const pokemon = document.createElement('div');
+    const label = document.createElement('span');
+    const img = document.createElement('img');
 
-        pokemon.appendChild(img);
-        pokemon.appendChild(label);
-        container.appendChild(pokemon);
-    }
+    pokemon.classList.add('pokemon');
+    label.innerText = `#${i}`;
+    label.innerText += `\n${pokemonName[i-1]}`;
+    img.src = `${baseURL}${i}.png`;
+
+    pokemon.appendChild(img);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
 }
-
-populatePokedex();
 
 /*
 <section>

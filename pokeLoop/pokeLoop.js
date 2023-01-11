707c8c5bf5d32ab0cfd08dc3d9e987a59477cd93
coriandar <88608040+coriandar@users.noreply.github.com>
2023-01-11 19:48:08 +1300

    Add create elements

--- pokeLoop/pokeLoop.js
+++ pokeLoop/pokeLoop.js
@@ -1,27 +1,54 @@
 // TODO:
-// between numbers
-// max load
-
-const container = document.querySelector('#container');
-const baseURL ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-
+// add name
+// make live form
 // let start = document.querySelector('input[name="start"]').value;
 // console.log(start)
 
-// loop error without parseInt, need sanitise
-let min = parseInt(prompt("min#:"));
-let max = parseInt(prompt("max#: "));
+function populatePokedex() {
+    // loop error without parseInt, need sanitise
+    let min = parseInt(prompt("min#:"));
+    let max = parseInt(prompt("max#: "));
 
-if (min > max) {
-    min = 1;
+    if (min > max) {
+        min = 1;
+    }
+    if (max > 151) {
+        max = 151;
+    }
+
+    addPokedexEntry(min, max);
 }
 
-if (max > 151) {
-    max = 151;
+function addPokedexEntry(min, max) {
+    const container = document.querySelector('#container');
+    const baseURL ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+    for (let i = min; i <= max; i++){
+        const pokemon = document.createElement('div');
+        const label = document.createElement('span');
+        const img = document.createElement('img');
+    
+        pokemon.classList.add('pokemon');
+        label.innerText = `#${i}`;
+        img.src = `${baseURL}${i}.png`;
+
+        pokemon.appendChild(img);
+        pokemon.appendChild(label);
+        container.appendChild(pokemon);
+    }
 }
 
-for (let i = min; i <= max; i++){
-    const pokemon = document.createElement('img');
-    pokemon.src = `${baseURL}${i}.png`;
-    container.append(pokemon);
-}
+populatePokedex();
+
+/*
+<section>
+    <div>
+        <img></img>
+        <span>i</span>
+    </div> 
+    <div>
+        <img></img>
+        <span>i</span>
+    </div> 
+</section>
+*/
